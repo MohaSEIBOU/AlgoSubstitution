8d58a8718d8ea86506b0884639a597663617181b
Mohamed SEIBOU MOUTAKILOU <88424959+MohaSEIBOU@users.noreply.github.com>
2022-11-23 01:12:14 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     /*Editer et implementer par SEIBOU Mohamed, Technique inspiré
       par wikipédia.      */
-//CODE DE CHIFFREMENT
+//CODE DE DECHIFFREMENT
     int i,j,k,n;
     vector<vector<char> > a(26,vector<char>(26));
     k=0;
@@ -15,26 +15,29 @@ int main(){
         for(j=0;j<n;j++){
             a[i][j]='A'+k;
             k++;
-            if(k==26)
-                k=0;
+            if(k==26) k=0;
         }
     }
-    cout<<"Entrer le message a chiffrer\n";
+    cout<<"Enter le  message a dechiffrer\n";
     string s;
     cin>>s;
-    cout<<"Entrer la cle\n";
+    cout<<"Enter la cle\n";
     string key;
     cin>>key;
     k=0;
-    int mod = key.size();
     for(i=key.size();i<s.size();i++){
-        key+=key[k%mod];
+        key+=key[k];
         k++;
     }
-    string encrypt;
+    string decrypt;
     for(i=0;i<s.size();i++){
-        encrypt+= a[s[i]-'A'][key[i]-'A'];
+        for(j=0;j<n;j++){
+            if(a[j][key[i]-'A']==s[i]){
+                decrypt += 'A'+j;
+                break;
+            }
+        }
     }
-    cout<<" Le message chiffrer est: "<<encrypt<<'\n';
+    cout<<"Le message dechiffrer est: "<<decrypt<<'\n';
     return 0;
 }
